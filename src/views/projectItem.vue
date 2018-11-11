a50224c2667cb26cbc103118dfe56a2507946513
wbrenn <11410392@mail.sustc.edu.cn>
2018-11-11 21:37:38 +0800

    wbr

--- src/views/projectItem.vue
+++ src/views/projectItem.vue
@@ -244,27 +244,11 @@
                 });
             },
             getNow() {
-                 // 获取当前日期
-            var date = new Date();
-
-            // 获取当前月份
-            var nowMonth = date.getMonth() + 1;
-
-            // 获取当前是几号
-            var strDate = date.getDate();
-
-            // 对月份进行处理，1-9月在前面添加一个“0”
-            if (nowMonth >= 1 && nowMonth <= 9) {
-            nowMonth = "0" + nowMonth;
-            }
-
-            // 对月份进行处理，1-9号在前面添加一个“0”
-            if (strDate >= 0 && strDate <= 9) {
-            strDate = "0" + strDate;
-            }
-
-            // 最后拼接字符串，得到一个格式为(yyyy-MM-dd)的日期
-            var nowDate = date.getFullYear() + nowMonth + strDate;
+                // 获取当前日期
+                var date = new Date();
+                // 最后拼接字符串，得到一个格式为(yyyy-MM-dd)的日期
+                var nowDate = date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate();
+                return nowDate
             }
         },
     created(){
@@ -302,8 +286,8 @@
                 var end_time = response.data.data.end_time
                 var data = that.getNow()
                 console.log(data)
-                var dateInt = parseInt(data)
-                var endInt = parseInt(end_time)
+                // var dateInt = parseInt(data)
+                // var endInt = parseInt(end_time)
                 if(data > end_time){
                     that.isOuttime = true
                 }
