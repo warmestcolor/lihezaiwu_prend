4300edb47f62303e682113c9ff0dfbb04a97ace8
wbrenn <11410392@mail.sustc.edu.cn>
2019-01-07 19:39:41 +0800

    fix

--- src/views/activityItem.vue
+++ src/views/activityItem.vue
@@ -215,16 +215,16 @@
             var shareUrl = "https://weixin.leaguervc.com/activity?id=" + that.$route.query.id
             var _url = ''
                 if (window.__wxjs_is_wkwebview === true) {
-                    _url = window.location.href
+                    _url = that.$store.state.entryURL
                     } else {
                     _url = shareUrl
                     }
-                console.log(_url)
+                console.log(window.location.href, _url)
             that.getRequest('/api/signature?noncestr=' + nonceStr + '&timestamp=' + timestamp + '&url=' + encodeURIComponent(_url))
                 .then(function (response) {
                     console.log(response);
                     wx.config({
-                        debug : true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        debug : false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                         appId : 'wx3f179d9e380457a5', // 必填，公众号的唯一标识
                         timestamp : timestamp, // 必填，生成签名的时间戳
                         nonceStr : nonceStr, // 必填，生成签名的随机串
